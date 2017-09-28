616bdd98bc03d9df74f1dea93d486d0aa1a16bc9
Nhat Pham <nhatphamcdn@gmail.com>
2017-09-28 14:39:48 +0700

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'Nhat Pham', email: 'nhatphamcdn@gmail.com', password: 'anhkin12', password_confirmation: 'anhkin12' )
